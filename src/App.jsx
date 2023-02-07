34123b67ae444712bf3a71f726072b4691e0d5c2
Jeremi131 <jeremijared10@gmail.com>
2023-02-07 18:19:04 -0400

    agrego footer, pantalla de carga, mejora en el input y agrego de sugerencias

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import './App.css'
+import Footer from './components/Footer'
 import LocationInfo from './components/LocationInfo'
 import ResidentInfo from './components/ResidentInfo'
 import getRandomLocation from './utils/getRandomLocation'
+import loading from '/src/assets/loading.png'
+import imageError from '/src/assets/imageError.png'
 
 function App() {
-  
+
   const [location, setLocation] = useState()
   const [numberLocation, setNumberLocation] = useState(getRandomLocation)
   const [hasError, setHasError] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const [listLocation, setListLocation] = useState()
+  const [isShow, setIsShow] = useState(false)
+  const [displaySuggestions, setDisplaySuggestions] = useState('none')
+  const [getValue, setGetValue] = useState()
+
 
   useEffect(() => {
     const url = `https://rickandmortyapi.com/api/location/${numberLocation}`
     axios.get(url)
-      .then(res =>{
+      .then(res => {
         setLocation(res.data)
         setHasError(false)
       })
@@ -22,32 +31,108 @@ function App() {
         console.log(err)
         setHasError(true)
       })
+      .finally(() => setTimeout(() => setIsLoading(false), 2000))
+
   }, [numberLocation])
-  
+
+
   const handleSubmit = e => {
     e.preventDefault()
-    if(e.target.inputLocation.value.trim().length === 0){
-            setNumberLocation(getRandomLocation())
+    if (e.target.inputLocation.value.trim().length === 0) {
+      setNumberLocation(getRandomLocation())
     } else {
-
+      
       setNumberLocation(e.target.inputLocation.value.trim())
     }
     e.target.inputLocation.value = e.target.inputLocation.value.trim()
+    setDisplaySuggestions('none')
+
   }
+
+  const handleChange = e => {
+    const url = `https://rickandmortyapi.com/api/location/?name=${e.target.value.trim()}`
+    axios.get(url)
+      .then(res => setListLocation(res.data.results))
+      .catch(err => console.log(err))
+      setDisplaySuggestions('flex')
+  }
+
+  const handleFocus = e => {
+    e.target.value = ""
+    clearInput()
+  }
+
+  // const handleBlur = () => {
+  //   setDisplaySuggestions('none')
+  // };
+
+  const handleClickList = (loc) => {
+    setNumberLocation(loc.id)
+    setDisplaySuggestions('none')
+    setGetValue(loc.name)
+  }
+
+  const clearInput = () => {
+    setGetValue(null);
+  };
+
 
   return (
     <div className="App">
-      <h1 className='app_tittle'>Rick and Morty App</h1>
-      <form className='form' onSubmit={handleSubmit}>
-        <input className='form_input' id='inputLocation' type="text" />
-        <button className='form_button'>Search</button>
-      </form>
+      {
+        isLoading ?
+          <div className='load_screen'>
+            <img className='load_image' src={loading} alt="" />
+          </div>
+
+          :
+
+          <>
+            <div className='img_app'>
+              <img className='logo' src="./assets/Rick_and_Morty.webp" alt="" />
+            </div>
+            <form className='form' onSubmit={handleSubmit}>
+              <input
+                className='form_input'
+                id='inputLocation'
+                type="text"
+                value={getValue}
+                onChange={handleChange}
+                onFocus={handleFocus} 
+                // onBlur={handleBlur}
+              />
+              <button className='form_button'>Search</button>
+              {
+                <ul className='suggestions' style={{ display: displaySuggestions }}>
+                  {
+                    listLocation?.map(loc => (
+                      <li className='list'  onClick={() =>  handleClickList(loc)} key={loc.id}>{loc.name}</li>
+                    ))
+                  }
+                </ul>
+              }
+            </form>
+          </>
+      }
+
       {
         hasError ?
-          <h2 className='error'>❌ Hey! you must provide an id from 1 to 126 🥺</h2>
-        :
+          <div className='error'>
+            <div className='text_error'>
+              <i className='bx box_error bx-x'></i>
+              <h2 className='tittle_error'>Hey! The information entered is incorrect</h2>
+              <i className='bx box_error bx-x'></i>
+            </div>
+
+            <img className='imageError' src={imageError} alt="" />
+          </div>
+          :
+
           <>
-            <LocationInfo location={location} />
+
+            <div className='location_container'>
+              <LocationInfo location={location} />
+            </div>
             <div className='residents_container'>
               {
                 location?.residents.map(url => (
@@ -58,8 +143,10 @@ function App() {
                 ))
               }
             </div>
+
           </>
       }
+      <Footer />
     </div>
   )
 }
--- src/components/ResidentInfo.jsx
+++ src/components/ResidentInfo.jsx
@@ -11,12 +11,12 @@ const ResidentInfo = ({url}) => {
         .then(res => setCharacter(res.data))
         .catch(err => console.log(err))
     }, [])
-    
+
   return (
     <article className='resident_info'>
         <header className='header'>
             <img className='character_img' src={character?.image} alt="" />
-            <div className='status_container'>
+            <div className={`status_container ${character?.status}_Shadow`}>
                 <span className={`status_circle ${character?.status}`}></span>
                 <span className='character_status'>{character?.status}</span>
             </div>     
@@ -30,15 +30,27 @@ const ResidentInfo = ({url}) => {
                 <li className='character_item'>
                   <span className='character_label'>Specie: </span>
                   {character?.species}
-                  </li>
+                </li>
+
+                <li className='character_item'>
+                  <span className='character_label'>Gender: </span>
+                  {character?.gender}
+                </li>
 
                 <li className='character_item'>
                   <span className='character_label'>Origin: </span>
-                  {character?.origin.name}</li>
+                  {character?.origin.name}
+                </li>
+
+                <li className='character_item'>
+                  <span className='character_label'>Type: </span>
+                  {character?.type}
+                </li>
 
                 <li className='character_item'>
                   <span className='character_label'>Episodes where appear: </span>
-                  {character?.episode.length}</li>
+                  {character?.episode.length}
+                </li>
             </ul>
         </section>
     </article>
